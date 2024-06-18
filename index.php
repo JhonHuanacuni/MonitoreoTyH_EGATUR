aac90e996c9f41f186ec0b78846df2c24798f230
JhonHuanacuni <jhon.huanacuni.mamani@gmail.com>
2024-06-18 11:46:49 -0500

    corrigiendo2.0

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css/style1.css">
+  <link rel="stylesheet" href="./css/style1.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
@@ -84,7 +84,7 @@ session_start();
 
           </div>
 
-          <input class="btn" type="submit" name="login" id="submit" value="INGRESAR">
+          <input class="btn button" type="submit" name="login" id="submit" value="INGRESAR">
 
 
           <div class="links">
